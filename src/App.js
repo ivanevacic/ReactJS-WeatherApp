96f0afdc9eed3145999781946e1be10cfc58cc01
ivanevacic <ivanevacicweb@gmail.com>
2018-02-27 20:27:03 +0100

    Fetching data from API works as intended

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import Form from './components/Form';
 //  Import Weather component
 import Weather from './components/Weather';
 
+//  OpenWeatherMap API key
+const API_KEY = '129410857ca7a65921957e105b006227';
+
 class App extends React.Component {
+  //  Function to fetch weather from API
+  getWeather = async (e) => {
+    //  Prevent default behavior of the component when we press button
+    e.preventDefault();
+    //  Fetch data from API using our API_KEY
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    //  Convert data we get from API to JSON format
+    const data = await api_call.json();
+    //  Console.log data we get to make sure it works
+    console.log(data);
+  }
   // Returns JSX
   render(){
     return (
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
         <Weather/>
       </div>
     );
