4d094f36a070bf280f8504fe8ef8f1b451feca19
ivanevacic <ivanevacicweb@gmail.com>
2018-02-27 20:40:07 +0100

    Fetch data with input values

--- src/App.js
+++ src/App.js
@@ -14,8 +14,12 @@ class App extends React.Component {
   getWeather = async (e) => {
     //  Prevent default behavior of the component when we press button
     e.preventDefault();
+    //  Value from Form input (<input type="text" name="city" placeholder="City.."/>)
+    const city = e.target.elements.city.value;
+    //  Value from Form input (<input type="text" name="country" placeholder="Country.."/>)
+    const country = e.target.elements.country.value;
     //  Fetch data from API using our API_KEY
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     //  Convert data we get from API to JSON format
     const data = await api_call.json();
     //  Console.log data we get to make sure it works
