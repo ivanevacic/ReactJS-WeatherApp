1ba55f497a0b7519312b2ad423be311f32207243
ivanevacic <ivanevacicweb@gmail.com>
2018-03-01 22:24:31 +0100

    Weather is now stateless component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-class Weather extends React.Component {
-    render() {
-        return(
-            <div>
-                {/* If both props.city and props.country return true,show City and Country on the page  */}
-                { this.props.city && <p>City: { this.props.city }</p> }
-                { this.props.country && <p>Country: { this.props.country }</p> }
-                { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
-                { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
-                { this.props.description && <p>Description: { this.props.description }</p> }
-                { this.props.error && <p>{ this.props.error }</p> }                                       
-            </div>
-        );
-    }
-}
+//  Stateless component
+    //  If we have one argument we don't need to use ()
+const Weather = props => (  
+    //  Can be written like this is we return one single element 
+        <div>
+            {/* If props.city returns true,show City and Country on the page  */}
+            { props.city && <p>City: { props.city }</p> }
+            { props.country && <p>Country: { props.country }</p> }
+            { props.temperature && <p>Temperature: { props.temperature }</p> }
+            { props.humidity && <p>Humidity: { props.humidity }</p> }
+            { props.description && <p>Description: { props.description }</p> }
+            { props.error && <p>{ props.error }</p> }                                       
+        </div>
+    );
 
 export default Weather;
