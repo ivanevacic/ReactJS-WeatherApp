1092d58abb9213f9e0f50e0d3414ae5cece297dd
ivanevacic <ivanevacicweb@gmail.com>
2018-03-01 22:28:24 +0100

    Fixed 'Form' component typo,Titles is now stateless component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Form = props => (
-    <form onSubmit={ this.props.getWeather }>
+    <form onSubmit={ props.getWeather }>
         <input type="text" name="city" placeholder="Enter city name..."/>
         <button>Get weather</button>
     </form>
