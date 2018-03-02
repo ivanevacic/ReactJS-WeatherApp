123bf87c228f6c3a65ffddd8649dd3b0e73a516e
ivanevacic <ivanevacicweb@gmail.com>
2018-03-02 18:00:46 +0100

    Imported Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//  Imported Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
